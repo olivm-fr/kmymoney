cb6b13d06a49d52cb201673dc260e6f5e98171a6
olivm38 <olivm38@home.fr>
2021-10-03 14:34:03 +0200

    loading dependencies fail...

---
+++ kmymoney/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "want a fix ? see https://github.com/Kitware/kwiver/commit/52b9c0ac78d8af096e5d141e693bff0c426441b8"
+LD_PRELOAD=/usr/lib64/qt5/plugins/kmymoney/weboob.so kmymoney
+
